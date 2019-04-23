11367ce2012f11ea8ce643ba49b2d9cbc4bbc673
[thisis-Solomon] <[solomonnjobvu1@gmail.com]>
2019-04-23 16:43:45 +0200

    basic Testing

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,19 @@
 
 import 'react-native';
 import React from 'react';
+import Weather from './component/Weather'
+import Forecast from './component/Forecast'
+import Weather_api from './component/Weather_api'
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Forecast from '../component/Forecast';
+import Weather_api from '../component/Weather_api';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+  renderer.create(<Weather/>)
+  renderer.create(<Forecast />);
+  renderer.create(<Weather_api/>)
 });
